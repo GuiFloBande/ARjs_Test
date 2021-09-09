7e7f7633659f5d464d126eee0e113cc5c434342e
GuiFloBande <85372616+GuiFloBande@users.noreply.github.com>
2021-09-09 22:48:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ var setModel = function(model, entity){
 
     entity.setAttribute('gltf-model', model.url);
 
-    const div = document.querySelector('.centered');
+    const div = document.querySelector('.instructions');
     //div.innerText = model.info;
     div.textContent = model.info;
 };

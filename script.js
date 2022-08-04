2531c201894303028b2e9c7d1e4fb20451a410d1
GuiFloBande <85372616+GuiFloBande@users.noreply.github.com>
2022-08-04 23:08:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -62,8 +62,8 @@ var setModel = function(model, entity){
     entity.setAttribute('gltf-model', model.url);
 
     const div = document.querySelector('.instructions');
-    //div.innerText = model.info;
-    div.textContent = model.info;
+    div.innerText = model.info;
+    //div.textContent = model.info;
 };
 
 function renderPlaces(places, btn) {
